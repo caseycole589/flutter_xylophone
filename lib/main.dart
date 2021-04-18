6eb62ba059cade6ea6365311e1296ec7480a62eb
casey cole <casey@capsinc.com>
2021-04-18 15:29:13 -0400

    added AudioPackage for playing sounds

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -8,7 +9,80 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: <Widget>[
+              TextButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note1.wav');
+                },
+                child: Text(''),
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.red,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note2.wav');
+                },
+                child: Text(''),
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.orange,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note3.wav');
+                },
+                child: Text(''),
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.yellow,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note4.wav');
+                },
+                child: Text(''),
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.green,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note5.wav');
+                },
+                child: Text(''),
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.teal,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note6.wav');
+                },
+                child: Text(''),
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note7.wav');
+                },
+                child: Text(''),
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.purple,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
